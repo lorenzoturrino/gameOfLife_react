b75877298c0fac56adc44f4b709318b90c205bad
LorenzoTurrino <lorenzo.turrino@gmail.com>
2016-05-30 18:19:26 +0100

    create grid

--- script.js
+++ script.js
@@ -13,13 +13,22 @@ var Cell = React.createClass({
   }
 });
 
+var gridSize = 10;
+
+var grid = [];
+for(var i = 0; i < gridSize; i++) {
+  var row = [];
+  for(var j = 0; j < gridSize; j++) {
+    row.push(<Cell />);
+  }
+  grid.push(<tr>{row}</tr>);
+}
+
 ReactDOM.render(
   <table className="mainGrid">
     <thead></thead>
     <tbody>
-      <tr>
-        <Cell />
-      </tr>
+        {grid}
     </tbody>
   </table>,
   document.getElementById('grid')
