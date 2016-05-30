248ba79069f7b3663e2ea3cc7b5fd3fc97b05ea1
LorenzoTurrino <lorenzo.turrino@gmail.com>
2016-05-30 17:56:08 +0100

    add cell object

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 var Cell = React.createClass({
-  getInitialState: function() {return null},
+  getInitialState: function() {
+    return {
+      alive: false
+    };
+  },
+  handleClick: function() {
+    this.setState({alive: !this.state.alive});
+  },
   render: function() {
-  return (<tr>hi</tr>);
-}
+    console.log("hi i'm rendering and i'm", this.state);
+  return <td data-alive={this.state.alive} onClick={this.handleClick}>cell proto</td>;
+  }
 });
 
 ReactDOM.render(
   <table className="mainGrid">
     <thead></thead>
     <tbody>
-      <Cell />
+      <tr>
+        <Cell />
+      </tr>
     </tbody>
   </table>,
   document.getElementById('grid')
-)
+);
