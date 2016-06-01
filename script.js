336964541495610ddd6a4c597b87bcd43b28b688
LorenzoTurrino <lorenzo.turrino@gmail.com>
2016-06-01 17:29:43 +0100

    implement stepper functions with terrible code

--- script.js
+++ script.js
@@ -16,37 +16,74 @@ var Grid = React.createClass({
     gridState[xpos][ypos] = !gridState[xpos][ypos];
     this.setState({gridState: gridState});
   },
+  setCellState: function(xpos, ypos, alive) {
+    var gridState = this.state.gridState;
+    gridState[xpos][ypos] = alive;
+    this.setState({gridState: gridState});
+  },
+  stepBuilder: function(oldGrid) {
+    console.log("building stepper");
+    return function(xpos, ypos) {
+      // console.log("sum stuff");
+      var sum = 0;
+      for(var i = xpos-1; i < xpos+1; i++) {
+        for(var j = ypos-1; j < ypos+1; j++) {
+          if(oldGrid[i] !== undefined && oldGrid[i][j] !== undefined) {
+            if(oldGrid[i][j] === true) {
+              sum += 1;
+            }
+          }
+        }
+      }
+      return true;
+      return survivalRules(sum, oldGrid[i][j]);
+    };
+  },
+  survivalRules: function(number, state) {
+    // a < 2 false
+    // a 2,3 true
+    // a > 3 false
+
+  },
+  stepGridState: function() {
+    // console.log("stepgrid", this.stepBuilder(this.state.gridState));
+    this.setState({gridState: this.createGrid(this.stepBuilder(this.state.gridState),3)});
+  },
   createGrid: function(buildFunction, gridSize) {
     var grid = [];
     for(var i = 0; i < gridSize; i++) {
       var row = [];
       for(var j = 0; j < gridSize; j++) {
+        // console.log("gridding", buildFunction);
         row.push(buildFunction(i,j));
       }
       grid.push(row);
     }
     return grid;
   },
-  stateBuilder: () => true,
+  stateBuilder: () => false,
   cellBuilder: function(xpos, ypos) {
     return (<Cell alive={this.state.gridState[xpos][ypos]} handleClick={this.toggleCellState} xPos={xpos} yPos={ypos}/>);
   },
   render: function() {
     return (
-      <tbody>
-        {
-          this.createGrid(this.cellBuilder,3)
-            .map(row => <tr>{row}</tr>)
-        }
-      </tbody>
+      <div>
+        <table className="grinding">
+          <thead></thead>
+          <tbody>
+            {
+              this.createGrid(this.cellBuilder,3)
+                .map(row => <tr>{row}</tr>)
+            }
+          </tbody>
+        </table>
+        <button onClick={this.stepGridState}>Step me</button>
+      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <table className="secondGrind">
-    <thead></thead>
-      <Grid />
-  </table>,
+  <Grid />,
   document.getElementById('grid')
 );
